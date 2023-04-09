dd1d44bc8c597fa91c35aad84ff17fcf11bd4120
Danilo Florenzano <danilo.maia@aedb.br>
2023-04-08 22:13:52 -0300

    [Feat] Adiciona handler para criacao de chaves dos filmes

---
+++ Domain/Entities/Bracketing.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public sealed class Bracketing
+    {
+        public Movie MovieLeft { get; }
+        public Movie? MovieRigth { get; }
+
+        public Bracketing(Movie movieLeft, Movie? movieRigth = null)
+        {
+            MovieLeft = movieLeft;
+            MovieRigth = movieRigth;
+        }
+    }
+}
